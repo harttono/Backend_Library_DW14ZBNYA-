d622bce49d65dbc43a1cb48efa15dadd473dadb2
harttono <hartono240896@gmail.com>
2020-12-02 16:41:15 +0700

    fix bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const router = require('./routes/router');
 const cors = require('cors');
-const Port = 5000;
+const Port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
